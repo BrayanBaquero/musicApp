e3898321aadf9f0599795b7af6fa49bd94f3a590
Brayan Baquero <brayan.baquero.cz@gmail.com>
2022-03-31 19:08:32 -0500

    Configuracion del proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.appmusic.com',
   appName: 'platzi-music',
   webDir: 'www',
   bundledWebRuntime: false
