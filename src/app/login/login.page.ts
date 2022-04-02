2f841c9c8693b00a32a7bd2701472f9f9fdb2407
Brayan Baquero <brayan.baquero.cz@gmail.com>
2022-04-02 13:35:29 -0500

    Validar inputs del formulario de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,13 +8,31 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  validation_messages={
+    email: [
+      {type:"required", message:"El email es requerido"},
+      {type:"pattern", message:"El email es invalido"}
+    ],
+    password:[
+      {type:"required", message:"El password es requerido"},
+      {type:"minlength", message:"El password debe contener mas de caracteres"}
+    ]
+  }
+
   constructor(private formBuilder: FormBuilder) { 
     this.loginForm=this.formBuilder.group({
       email:new FormControl("",Validators.compose([
         Validators.required,
         Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+      ])),
+      password:new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
       ]))
-    })
+
+    });
+
+    console.log(this.loginForm)
   }
 
   loginUser(loginForm){
